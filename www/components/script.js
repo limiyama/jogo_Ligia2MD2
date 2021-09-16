32e8d404c496ec7ed641c1607b28212ce11b75e4
mimiyama <lmizuyama@gmail.com>
2021-09-17 03:02:19 +0900

    a

--- www/components/script.js
+++ www/components/script.js
@@ -1,11 +1,29 @@
 // This is a JavaScript file
 window.onload = function(){
   inicioJogo();
+
+  document.querySelector("#esquerda").addEventListener("click", function(){
+    esquerda();
+  });
+
+  document.querySelector("#subir").addEventListener("click", function(){
+    subir();
+  });
+
+  document.querySelector("#descer").addEventListener("click", function(){
+    descer();
+  });
+
+  document.querySelector("#direita").addEventListener("click", function(){
+    direita();
+  });
 }
+
+var personagemObj;
 
 function inicioJogo(){
   areaJogo.start();
-  personagem = componente("#F00", 10, 120, 30, 30);
+  personagemObj = new componente("#F00", 10, 120, 30, 30);
 }
 
 let areaJogo = {
@@ -14,6 +32,10 @@ let areaJogo = {
     this.canvas.height = 300,
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.intervalo = setInterval(atualizaAreaJogo, 20);
+  },
+  limpar: function(){
+    this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
   }
 }
 
@@ -22,8 +44,37 @@ function componente(cor, x, y, largura, altura){
   this.largura = largura,
   this.x = x,
   this.y = y,
+  this.velocidadeX = 0;
+  this.velocidadeY = 0;
+  this.atualiza = function(){
   contexto = areaJogo.context;
   contexto.fillStyle = cor, 
   contexto.fillRect(this.x, this.y, this.altura, this.largura);
+  },
+  this.novaPosicao = function(){
+    this.x += this.velocidadeX;
+    this.y += this.velocidadeY;
+  }
+}
 
+function atualizaAreaJogo(){
+  areaJogo.limpar();
+  personagemObj.novaPosicao();
+  personagemObj.atualiza();
+}
+
+function subir(){
+  personagemObj.velocidadeY -= 1;
+}
+
+function descer(){
+  personagemObj.velocidadeY += 1;
+}
+
+function direita(){
+  personagemObj.velocidadeX += 1;
+}
+
+function esquerda(){
+  personagemObj.velocidadeX -= 1;
 }
